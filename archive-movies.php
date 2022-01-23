ebe91ee31c76596e7b14583c9de57d20989f178f
arhibober <arhibober@gmail.com>
2022-01-23 23:20:33 +0200

    pagination added

--- archive-movies.php
+++ archive-movies.php
@@ -1,9 +1,14 @@
-<?php get_header(); ?>
+<?php
+/*
+ * Template Name: Movies archive
+ * Template post type: page
+ */
+get_header(); ?>
 <style>
-    img.attachment-post-thumbnail.size-post-thumbnail.wp-post-image{
-        height: auto;
-        width: auto;
-    }
+img.attachment-post-thumbnail.size-post-thumbnail.wp-post-image {
+    height: auto;
+    width: auto;
+}
 </style>
 <main>
     <div class="container">
@@ -11,38 +16,41 @@
             <div class="col-md-3">
                 <div class="position-sticky" style="top: 2rem;">
                     <script>
-                        function dynamicChangeNum() {
-                            var rng=document.getElementById('movie_imdb_rating');
-                            var p=document.getElementById('one');
-                            p.innerHTML=rng.value;
-                        }
+                    function dynamicChangeNum() {
+                        var rng = document.getElementById('movie_imdb_rating');
+                        var p = document.getElementById('one');
+                        p.innerHTML = rng.value;
+                    }
                     </script>
                     <style>
-                        input#movie_imdb_rating {
-                            margin-right: 5px;
-                        }
+                    input#movie_imdb_rating {
+                        margin-right: 5px;
+                    }
                     </style>
-                    <form action="<?php echo get_post_type_archive_link( 'movies' ); ?>" method="get">
-                    <div class="mb-3">
-                        <label for="filter_search" class="form-label">Sort by</label>
-                        <select name="sorting" class="form-select form-select-sm" aria-label=".form-select-sm example">
-                            <option selected>Open this select menu</option>
-                            <option value="AZ">Name A-Z</option>
-                            <option value="release_new">Release date (from new to old)</option>
-                            <option value="release_old">Release date (from old to new)</option>
-                        </select>
-                        <button type="submit" class="btn btn-primary mt-3">Sort</button>
-                    </div>
+                    <form action="<?php echo get_post_type_archive_link('movies'); ?>" method="get">
+                        <div class="mb-3">
+                            <label for="filter_search" class="form-label">Sort by</label>
+                            <select name="sorting" class="form-select form-select-sm"
+                                aria-label=".form-select-sm example">
+                                <option selected>Open this select menu</option>
+                                <option value="AZ">Name A-Z</option>
+                                <option value="release_new">Release date (from new to old)</option>
+                                <option value="release_old">Release date (from old to new)</option>
+                            </select>
+                            <button type="submit" class="btn btn-primary mt-3">Sort</button>
+                        </div>
                     </form>
-                    <form action="<?php echo get_post_type_archive_link( 'movies' ); ?>" method="get">
+                    <form action="<?php echo get_post_type_archive_link('movies'); ?>" method="get">
                         <div class="mb-3">
                             <label for="filter_search" class="form-label">Search</label>
-                            <input type="text" name="filter_search" class="form-control" id="filter_search" value="<?php echo $_GET['filter_search']; ?>">
+                            <input type="text" name="filter_search" class="form-control" id="filter_search"
+                                value="<?php echo $_GET['filter_search']; ?>">
                         </div>
                         <div class="mb-3">
                             <label for="filter_relevance" class="form-label">IMDb Rating </label>
                             <div class="d-flex">
-                                <input type="range" oninput="dynamicChangeNum()" id="movie_imdb_rating" name="movie_imdb_rating" min="0" max="10" value="">
+                                <input type="range" oninput="dynamicChangeNum()" id="movie_imdb_rating"
+                                    name="movie_imdb_rating" min="0" max="10" value="">
                                 <div id="one"></div>
                             </div>
                         </div>
@@ -55,22 +63,52 @@
             </div>
             <div class="col-md-9">
                 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                    <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-                        <div class="col">
-                            <a style="text-decoration: none; color: black;" href="<?php the_permalink(); ?>">
-                                <div class="card shadow-sm">
-                                    <?php the_post_thumbnail(); ?>
-                                    <div class="card-body">
-                                        <h3 class="mb-0"><?php the_title(); ?></h3>
-                                    </div>
+                    <?php if (have_posts()): while (have_posts()): the_post(); ?>
+                    <div class="col">
+                        <a style="text-decoration: none; color: black;" href="<?php the_permalink(); ?>">
+                            <div class="card shadow-sm">
+                                <?php the_post_thumbnail(); ?>
+                                <div class="card-body">
+                                    <h3 class="mb-0"><?php the_title(); ?></h3>
                                 </div>
-                            </a>
-                        </div>
-                    <?php endwhile; endif;?>
+                            </div>
+                        </a>
+                    </div>
+                    <?php endwhile;endif; ?>
                 </div>
             </div>
+            <div class="col-md-9">
+                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                    <?php
+global $wp_query, $page;
+$current_page = (get_query_var('paged')) ? get_query_var('paged') : 1;
+$custom_query_args = array
+    (
+    "post_type" => "movies",
+    "post_status" => "publish",
+    "paged" => $current_page,
+);
+$custom_query = new WP_Query($custom_query_args);
+$wp_query = null;
+$wp_query = $custom_query;
+while (have_posts()): the_post(); ?>
+                    <div class="col">
+                        <a style="text-decoration: none; color: black;" href="<?php the_permalink(); ?>">
+                            <div class="card shadow-sm">
+                                <?php the_post_thumbnail(); ?>
+                                <div class="card-body">
+                                    <h3 class="mb-0"><?php the_title(); ?></h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <?php endwhile; ?>
+                </div>
+            </div>
+            <?php
+do_action('numeric_pagination');
+?>
         </div>
     </div>
 </main>
 <?php get_footer(); ?>
-
