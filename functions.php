8976a95a5e56d58f865aac77ab34abee283b57bb
Yelysei Bocharov <ybocharov@stud.khpi.ucode-connect.study>
2022-01-29 20:48:40 +0200

    Part#1

--- functions.php
+++ functions.php
@@ -21,5 +21,25 @@ new client_site\classes\theme\DefaultFunctionsTheme();
 new client_site\classes\modules\CustomFilterCPT();
 new  client_site\classes\modules\RegisterCPT();
 new \client_site\classes\modules\CustomPagination();
-//new client_site\classes\modules\API();
+use client_site\classes\modules\API;
 
+add_action('customAddContent', addedPost());
+
+function addedPost(){
+    $array = (new API) -> request();
+
+    $films = $array->data;
+    foreach($films as $film){
+        $defaults = array(
+            'post_author'   => 1,
+            'post_title'    => $film->title,
+            'post_content'  => $film->description,
+            'post_status'   => 'publish',
+            'post_type'     => 'movies',
+            'meta_input'    => ['url_img'   => $film->poster_path,
+                                'IDBM'      => $film->vote_average]
+
+        );
+        $id = wp_insert_post($defaults);
+    }
+}
